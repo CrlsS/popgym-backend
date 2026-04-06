9ae7206c8c5eb6c29ba5cae58fd53222e9f07eb8
CrlsS <carl.sabom@gmail.com>
2026-04-06 20:01:17 +0200

    Create signup.js

---
+++ api/signup.js
@@ -0,0 +1,33 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).end();
+  }
+
+  const data = req.body;
+
+  try {
+    const response = await fetch("https://api.billecta.com/v1/debtors/debtor", {
+      method: "POST",
+      headers: {
+        "Authorization": "Basic " + Buffer.from(process.env.BILLECTA_KEY + ":").toString("base64"),
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: data.name,
+        email: data.email,
+        ssn: data.ssn,
+        address: {
+          street: data.street,
+          zip: data.zip,
+          city: data.city
+        }
+      })
+    });
+
+    const result = await response.json();
+    res.status(200).json(result);
+
+  } catch (error) {
+    res.status(500).json({ error: "Something went wrong" });
+  }
+}
